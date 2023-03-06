21d0cda462fcae45d572b33cc0b47fc88d0fe343
Stuepp <arthurh.c@hotmail.com>
2023-03-06 11:47:03 -0300

    start of the layout

--- gravadora/members/views.py
+++ gravadora/members/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+def members(request):
+    template = loader.get_template('index.htm')
+    return HttpResponse(template.render())
