34ab4b72dea958329c6fc4628f182eb67512cefa
Stuepp <arthurh.c@hotmail.com>
2023-03-13 13:12:28 -0300

    bd-modelo-relacional

--- bd/gravadora.sql
+++ bd/gravadora.sql
@@ -5,15 +5,19 @@ create table musico(
 );
 
 create table instrumento(
-	codigo int primary key,
+	id_instrumento int primary key,
 	nome varchar(50) not null
 );
 
 create table disco(
-	identificador int primary key,
+	id_disco int primary key,
 	titulo varchar(75),
 	formato varchar(75),
-	data_d date
+	data date,
+	fk_musico int,
+	fk_banda int,
+	foreign key(fk_musico) references musico,
+	foreign key(fk_banda) references banda
 );
 
 create table produtor(
@@ -30,6 +34,14 @@ create table musica(
 create table banda(
 	id_banda int primary key
 	nome varchar(75) not null
+);
+
+create table own(
+	id_own int primary key,
+	fk_musico int,
+	fk_banda int,
+	foreign key(fk_musico) references musico,
+	foreign key(fk_banda) references banda
 );
 
 create table toca(
@@ -56,12 +68,18 @@ create table aparece(
 	foreign key(fk_musica) references musica
 );
 
-create table participa(
-	id_participa int primary key,
+create table esta(
+	id_esta int primary key,
 	fk_musico int,
 	fk_banda int,
+	foreign key(fk_musico) references musico,
+	foreign key(fk_banda) references banda
+);
+
+create table participa(
+	id_participa int primary key,
+	fk_own int,
 	fk_musica int,
-	foreign key(fk_musico) references(musico),
-	foreign key(fk_banda) references(banda),
-	foreign key(fk_musica) references(musica)
+	foreign key(fk_own) references own,
+	foreign key(fk_musica) references musica
 );
